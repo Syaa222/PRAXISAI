ab33f1462835d74b523c38d87abd5013bd09abbc
Syaa222 <syaa222.px@gmail.com>
2024-07-30 14:32:25 +0700

    Calculator

---
+++ operators.py
@@ -0,0 +1,35 @@
+#assignment operators
+a = 5 
+b = 3 
+
+# x += 3 is same as x= x+3
+a += 4
+
+print (a)
+
+a = 5 
+b = 3 
+
+# a+=3 is same as a=a+3
+
+#before
+print (a)
+print("first modulus: ", a % b)
+
+a += 4
+
+#after
+print (a) # now a is equal to 9
+
+print("Second modulus: ", a % b)
+
+#exponent
+print( a**2) # a 
+
+# floor division 
+print("floor division: ", 5 // b) # dibulatkan sebagai int
+print("Division: ", 5 // 3) # float result
+
+
+
+
---
+++ trycalculator.py
@@ -0,0 +1,7 @@
+# Kalkulator Penjumlahan
+print ("kalkulator penjumlahan")
+
+num1 = input ("Insert the first number")
+num2 = input ("Insert the second number")
+
+print(int(num1) + int(num2))
