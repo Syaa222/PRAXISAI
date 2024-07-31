79357f9a1a452cc1e300a4bec4e2f1169999deff
Syaa222 <syaa222.px@gmail.com>
2024-07-31 11:05:20 +0700

    New Lesson If

---
+++ 01-03-Conditionals/ifelse.py
@@ -0,0 +1,20 @@
+user = "Andrelya"
+
+user_in = input ("Input the user name: ")
+
+if user_in == user:
+    print("welcome to this app!")
+else:
+    print ("You are a stranger!")
+
+user = "Hate"
+user2 = "Ranna"
+
+user_in = input("Input the user name: ")
+
+if user_in == user:
+    print("Welcome to this app!")
+elif user_in == user2:
+    print("Welcome Budi!")
+else:
+    print("You are a stranger!")
