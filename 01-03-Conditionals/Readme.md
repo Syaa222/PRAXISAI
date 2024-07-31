b47033b0e13bb08a6463c50f6d0b67757fa2d333
Syaa222 <syaa222.px@gmail.com>
2024-07-31 11:55:07 +0700

    KALKULATOR GENERAL

--- 01-03-Conditionals/Readme.md
+++ 01-03-Conditionals/Readme.md
@@ -2,5 +2,18 @@
 This is a code for a simple multi-purpose calculator
 ## Algorithm
 1. The app shows welcome display
+    ("Selamat datang di kalkulator blue!")
 2. The app asks for user's option
-3. If 
+    Masukkan pilihan operasi:
+    a. Pertambahan
+    b. Pengurangan 
+    c. Perkalian
+    d. Pembagian
+3. The app asks for user's, The first number
+    ("Enter the first number")
+4. The app asks for user's, The second number 
+    ("Enter the second number")
+5. Gift the result for Addition
+6. End
+
+#
---
+++ 01-03-Conditionals/Trycalculator#2.py
@@ -0,0 +1,27 @@
+# Selamat datang di kalkulator blue!
+print ("Selamat datang di kalkulator blue!")
+
+num1 = input ("Insert the first number")
+num2 = input ("Insert the second number")
+
+select = input("Choose the option")
+
+if select == "a":
+    print(int(num1) + int(num2))
+    #addision
+
+elif select == "d":
+    print(int(num1) - int(num2))
+    #Subtraction
+
+elif select == "c":
+    print(int(num1) * int(num2))
+    #Multipication
+
+elif select == "p":
+    print(int(num1) / int(num2))
+    #Division
+
+
+
+
